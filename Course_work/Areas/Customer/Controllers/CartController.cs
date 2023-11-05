e70afa4c3096c84aa9f0ba03fff4468b1652ae58
taraskvyk-lpnu <taras.kvyk.pz.2022@lpnu.ua>
2023-11-05 18:37:26 +0200

    Placing older impleneted

--- Course_work/Areas/Customer/Controllers/CartController.cs
+++ Course_work/Areas/Customer/Controllers/CartController.cs
@@ -106,5 +106,93 @@ namespace BooksWeb.Areas.Customer.Controllers
 
             return View(ShoppingCartVM);
         }
-    }
+
+		[HttpPost]
+		[ActionName("Summary")]
+		public IActionResult SummaryPOST(ShoppingCartVM shoppingCartVM)
+		{
+			//var claimsIdentity = (ClaimsIdentity)User.Identity; // Отримуємо Id користувач, що зараз в акаунті
+			//var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+			ShoppingCartVM.ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(/*u => u.ApplicationUserId == userId, */includeProperties: "Book");
+
+			//ApplicationUser applicationUser = _unitOfWork.ApplicationUsers.Get(u => u.Id == userId);
+
+			ShoppingCartVM.OrderHeader.OrderDate = DateTime.Now;
+			//ShoppingCartVM.OrderHeader.ApplicationUserId = userId;
+
+			foreach (var cart in ShoppingCartVM.ShoppingCartList)
+			{
+				ShoppingCartVM.OrderHeader.OrderTotal += (cart.Price * cart.Count);
+			}
+
+			_unitOfWork.OrderHeader.Add(ShoppingCartVM.OrderHeader);
+			_unitOfWork.Save();
+
+			foreach (var cart in ShoppingCartVM.ShoppingCartList)
+			{
+				OrderDetail orderDetail = new OrderDetail()
+				{
+					BookId = cart.BookId,
+					OrderHeaderId = ShoppingCartVM.OrderHeader.Id,
+					Price = cart.Price,
+					Count = cart.Count
+				};
+
+				_unitOfWork.OrderDetail.Add(orderDetail);
+				_unitOfWork.Save();
+			}
+
+			//if (applicationUser.CompanyId.GetValueOrDefault() == 0)
+			//{
+			//	// Stripe logic
+			//	var domain = "https://localhost:7033/";
+
+			//	var options = new SessionCreateOptions
+			//	{
+			//		SuccessUrl = domain + $"customer/cart/OrderConfirmation?id={ShoppingCartVM.OrderHeader.Id}",
+			//		CancelUrl = domain + "customer/cart/index",
+			//		LineItems = new List<SessionLineItemOptions>(),
+			//		Mode = "payment",
+			//	};
+
+			//	foreach (var item in ShoppingCartVM.ShoppingCartList)
+			//	{
+			//		var SessionLineItem = new SessionLineItemOptions
+			//		{
+			//			PriceData = new SessionLineItemPriceDataOptions()
+			//			{
+			//				UnitAmount = (long)(item.Price * 100),
+			//				Currency = "usd",
+			//				ProductData = new SessionLineItemPriceDataProductDataOptions()
+			//				{
+			//					Name = item.Product.Title
+			//				}
+			//			},
+
+			//			Quantity = item.Count
+			//		};
+
+			//		options.LineItems.Add(SessionLineItem);
+			//	}
+
+			//	var service = new SessionService();
+			//	Session session = service.Create(options);
+
+			//	_unitOfWork.OrderHeader.UpdateStripePaymentId(ShoppingCartVM.OrderHeader.Id, session.Id, session.PaymentIntentId);
+			//	_unitOfWork.Save();
+
+			//	Response.Headers.Add("Location", session.Url);
+			//	return new StatusCodeResult(303);
+			//}
+
+			ShoppingCartVM.OrderHeader.TrackingNumber = ShoppingCartVM.OrderHeader.Id.ToString();
+			_unitOfWork.ShoppingCart.RemoveRange(ShoppingCartVM.ShoppingCartList);
+			_unitOfWork.Save();
+			
+			TempData["success"] = $"Order \"{ShoppingCartVM.OrderHeader.TrackingNumber}\" has been placed successfully";
+			
+			return RedirectToAction(nameof(Index), "Home");
+		}
+	}
 }
